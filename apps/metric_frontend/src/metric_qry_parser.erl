8d26226cb450974eb7ab0e75773c6a7aab72f491
Heinz N. Gies <heinz@licenser.net>
2014-06-24 04:20:37 +0200

    Fixed LAST

--- apps/metric_frontend/src/metric_qry_parser.erl
+++ apps/metric_frontend/src/metric_qry_parser.erl
@@ -38,7 +38,7 @@ range(["LAST", A, "S" | L]) ->
     {Mega, Sec, _Micro} = now(),
     Now = Mega * 1000000  + Sec,
     Ad = i(A),
-    metric(L, {range, Now - Ad, Now});
+    metric(L, {range, Now - Ad, Ad});
 range(L) ->
     {error, undefeind, L}.
 
