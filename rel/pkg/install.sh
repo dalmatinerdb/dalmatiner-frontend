ec8bce7f5c349c15adae7ca1cf058399161ba8f4
Heinz N. Gies <heinz@licenser.net>
2015-03-18 14:14:50 +0100

    updated install file

--- rel/pkg/install.sh
+++ rel/pkg/install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+AWK=/usr/bin/awk
+SED=/usr/bin/sed
+
 USER=dalmatiner
 GROUP=$USER
 
@@ -40,13 +43,13 @@ case $2 in
         echo Importing service ...
         svccfg import /opt/local/dalmatinerfe/share/dfe.xml
         echo Trying to guess configuration ...
-        IP=`ifconfig net0 | grep inet | awk -e '{print $2}'`
+        IP=`ifconfig net0 | grep inet | $AWK '{print $2}'`
         CONFFILE=/opt/local/dalmatinerfe/etc/dalmatinerfe.conf
         if [ ! -f "${CONFFILE}" ]
         then
             echo "Creating new configuration from example file."
             cp ${CONFFILE}.example ${CONFFILE}
-            sed --in-place -e "s/127.0.0.1/${IP}/g" ${CONFFILE}
+            $SED -i bak -e "s/127.0.0.1/${IP}/g" ${CONFFILE}
         else
             echo "Merging old file with new template, the original can be found in ${CONFFILE}.old."
             /opt/local/dalmatinerfe/share/update_config.sh ${CONFFILE}.example ${CONFFILE} > ${CONFFILE}.new &&
