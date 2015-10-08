e99caab95bdf9ad6bfcf86627319a9bc9b853fcb
Heinz N. Gies <heinz@licenser.net>
2015-10-08 10:40:42 +0200

    Fully remove config update scirpt

--- rel/pkg/install.sh
+++ rel/pkg/install.sh
@@ -8,13 +8,6 @@ GROUP=$USER
 
 case $2 in
     PRE-INSTALL)
-        #if grep '^Image: base64 1[34].[1234].*$' /etc/product
-	#then
-	#    echo "Image version supported"
-	#else
-	#    echo "This image version is not supported please use the base64 13.2.1 image."
-	#    exit 1
-        #fi
         if grep "^$GROUP:" /etc/group > /dev/null 2>&1
         then
             echo "Group already exists, skipping creation."
@@ -51,9 +44,8 @@ case $2 in
             cp ${CONFFILE}.example ${CONFFILE}
             $SED -i bak -e "s/127.0.0.1/${IP}/g" ${CONFFILE}
         else
-            echo "Config file exists you might need to update settings"
-            #echo "Merging old file with new template, the original can be found in ${CONFFILE}.old."
-            #/opt/local/dalmatinerfe/share/update_config.sh ${CONFFILE}.example ${CONFFILE} > ${CONFFILE}.new &&
+            echo "Please make sure you update your config according to the update manual!"
+            #/opt/local/fifo-sniffle/share/update_config.sh ${CONFFILE}.example ${CONFFILE} > ${CONFFILE}.new &&
             #    mv ${CONFFILE} ${CONFFILE}.old &&
             #    mv ${CONFFILE}.new ${CONFFILE}
         fi
