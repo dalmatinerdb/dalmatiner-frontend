b8887c1d2665f78f7f9c5883be08f64449d1dc2e
Heinz N. Gies <heinz@licenser.net>
2015-11-25 22:46:29 +0100

    Linting and cleanup

--- Makefile
+++ Makefile
@@ -2,13 +2,9 @@ REBAR = $(shell pwd)/rebar3
 
 .PHONY: deps rel stagedevrel package version all tree
 
-all: cp-hooks compile update
+all: version_header compile
 
-update:
-	$(REBAR) update
-
-cp-hooks:
-	cp hooks/* .git/hooks
+include fifo.mk
 
 version:
 	@echo "$(shell git symbolic-ref HEAD 2> /dev/null | cut -b 12-)-$(shell git log --pretty=format:'%h, %ad' -1)" > dalmatiner_frontend.version
@@ -16,44 +12,7 @@ version:
 version_header: version
 	@echo "-define(VERSION, <<\"$(shell cat dalmatiner_frontend.version)\">>)." > apps/dalmatiner_frontend/include/dalmatiner_frontend_version.hrl
 
-compile: update version_header
-	$(REBAR) compile
-
 clean:
 	$(REBAR) clean
 	make -C rel/pkg clean
 	-rm -r apps/*/ebin
-
-qc:
-	$(REBAR) as eqc eqc
-
-eunit: 
-	$(REBAR) eunit
-
-test: eunit
-	$(REBAR) xref
-
-rel: all
-	$(REBAR) as prod release
-
-package: rel
-	make -C rel/pkg package
-
-###
-### Docs
-###
-docs:
-	$(REBAR) doc
-
-##
-## Developer targets
-##
-
-xref: all
-	$(REBAR) xref
-
-tree:
-	rebar3 tree | grep -v '=' | sed 's/ (.*//' > tree
-
-tree-diff: tree
-	git diff test -- tree
--- apps/dalmatiner_frontend/src/dalmatiner_idx_handler.erl
+++ apps/dalmatiner_frontend/src/dalmatiner_idx_handler.erl
@@ -3,31 +3,17 @@
 %% @doc POST echo handler.
 -module(dalmatiner_idx_handler).
 
--export([init/3, handle/2, terminate/3]).
+-export([send/4, content_type/1, init/3, handle/2, terminate/3]).
 
 -ignore_xref([init/3, handle/2, terminate/3]).
 
 init(_Transport, Req, []) ->
     {ok, Req, undefined}.
 
-content_type([]) ->
-    other;
-content_type([{{<<"text">>,<<"html">>,_},_,_} | _]) ->
-    html;
-content_type([{{<<"application">>,<<"xhtml+xml">>,_},_,_} | _]) ->
-    html;
-content_type([{{<<"application">>,<<"json">>,_},_,_} | _]) ->
-    json;
-content_type([{{<<"application">>,<<"msgpack">>,_},_,_} | _]) ->
-    msgpack;
-content_type([{{<<"application">>,<<"x-msgpack">>,_},_,_} | _]) ->
-    msgpack;
-content_type([_ | R]) ->
-    content_type(R).
-
 -dialyzer({no_opaque, handle/2}).
 handle(Req, State) ->
-    Req0 = cowboy_req:set_resp_header(<<"access-control-allow-origin">>, <<"*">>, Req),
+    Req0 = cowboy_req:set_resp_header(<<"access-control-allow-origin">>,
+                                      <<"*">>, Req),
     case cowboy_req:qs_val(<<"q">>, Req0) of
         {undefined, Req1} ->
             F = fun (Socket, Transport) ->
@@ -46,7 +32,6 @@ handle(Req, State) ->
                         cowboy_req:reply(400, [], Error, Req1),
                     {ok, Req2, State};
                 {T, {ok, Start, R2}} ->
-                    {ok, A, Req2} = cowboy_req:parse_header(<<"accept">>, Req1),
                     R3 = [[{<<"n">>, Name},
                            {<<"r">>, Resolution},
                            {<<"v">>, mmath_bin:to_list(Data)}]
@@ -54,25 +39,45 @@ handle(Req, State) ->
                     D = [{<<"s">>, Start},
                          {<<"t">>, T},
                          {<<"d">>, R3}],
-                    case content_type(A) of
-                        json ->
-                            {ok, Req3} =
-                                cowboy_req:reply(
-                                  200, [{<<"content-type">>, <<"application/json">>}],
-                                  jsx:encode(D), Req2),
-                            {ok, Req3, State};
-                        msgpack ->
-                            {ok, Req3} =
-                                cowboy_req:reply(
-                                  200, [{<<"content-type">>, <<"application/x-msgpack">>}],
-                                  msgpack:pack(D, [jsx]), Req2),
-                            {ok, Req3, State};
-                        _ ->
-                            {ok, Req2} = cowboy_req:reply(415, Req1),
-                            {ok, Req2, State}
-                    end
+                    {ContentType, Req2} = content_type(Req1),
+                    send(ContentType, D, Req2, State)
             end
     end.
+
+content_type(Req) ->
+    {ok, A, Req1} = cowboy_req:parse_header(<<"accept">>, Req),
+    {content_type_(A), Req1}.
+
+content_type_([]) ->
+    other;
+content_type_([{{<<"text">>, <<"html">>, _}, _, _} | _]) ->
+    html;
+content_type_([{{<<"application">>, <<"xhtml+xml">>, _}, _, _} | _]) ->
+    html;
+content_type_([{{<<"application">>, <<"json">>, _}, _, _} | _]) ->
+    json;
+content_type_([{{<<"application">>, <<"msgpack">>, _}, _, _} | _]) ->
+    msgpack;
+content_type_([{{<<"application">>, <<"x-msgpack">>, _}, _, _} | _]) ->
+    msgpack;
+content_type_([_ | R]) ->
+    content_type_(R).
+
+send(json, D, Req, State) ->
+    {ok, Req1} =
+        cowboy_req:reply(
+          200, [{<<"content-type">>, <<"application/json">>}],
+          jsx:encode(D), Req),
+    {ok, Req1, State};
+send(msgpack, D, Req, State) ->
+    {ok, Req1} =
+        cowboy_req:reply(
+          200, [{<<"content-type">>, <<"application/x-msgpack">>}],
+          msgpack:pack(D, [jsx]), Req),
+    {ok, Req1, State};
+send(_, _D, Req, State) ->
+    {ok, Req1} = cowboy_req:reply(415, Req),
+    {ok, Req1, State}.
 
 terminate(_Reason, _Req, State) ->
     {ok, State}.
