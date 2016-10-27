d6b4e0802b0b3bf347d208ef8995bd62fd755090
Heinz N. Gies <heinz@licenser.net>
2016-10-27 11:11:09 +0200

    Improve function list and split it into more sections

--- apps/dalmatiner_frontend/priv/static/js/functions.js
+++ apps/dalmatiner_frontend/priv/static/js/functions.js
@@ -1,4 +1,23 @@
 $(function() {
+  function is_hist(e) {
+    if (e.return == "histogram")
+      return true;
+    for (i=0; i < e.signature.length; i++) {
+      var s = e.signature[i];
+      if (s == "histogram")
+        return true;
+    }
+    return false;
+  };
+  function is_aggr(e) {
+    for (i=0; i < e.signature.length; i++) {
+      var s = e.signature[i];
+      if (s == "time")
+        return true;
+    }
+    return false;
+  };
+
   msgpack.download("/functions", {header: {accept:"application/x-msgpack"}}, function(d) {
     d.sort(function (a, b) {
       if (a.name > b.name) {
@@ -11,19 +30,32 @@ $(function() {
       return 0;
     });
     d.forEach(function(e) {
-      if (e.combiner_type == "none") {
+      if (is_hist(e)) {
+        $("#hist_funs").append("<tr>" +
+                               "<td>" + e.name +"</td>"+
+                               "<td>" + e.desc +"</td>"+
+                               "<td>" + e.help +"</td>"+
+                               "</tr>")
+      } else if (is_aggr(e)) {
         $("#aggr_funs").append("<tr>" +
                                "<td>" + e.name +"</td>"+
                                "<td>" + e.desc +"</td>"+
                                "<td>" + e.help +"</td>"+
                                "</tr>")
-      } else {
+      } else if (e.combiner_type != "none") {
+
         $("#comb_funs").append("<tr>" +
                                "<td>" + e.name +"</td>"+
                                "<td>" + e.desc +"</td>"+
                                "<td>" + e.help +"</td>"+
                                "</tr>")
 
+      } else {
+        $("#trans_funs").append("<tr>" +
+                                "<td>" + e.name +"</td>"+
+                                "<td>" + e.desc +"</td>"+
+                                "<td>" + e.help +"</td>"+
+                                "</tr>")
       }
     })
   })
